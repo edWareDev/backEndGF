eec613eed99a8d28571f234e71006e83831a68af
Edward Espinoza <em.esc.et@gmail.com>
2023-06-05 16:42:30 -0500

    Fix api get method

--- src/controllers/api/customers.get.controller.js
+++ src/controllers/api/customers.get.controller.js
@@ -2,6 +2,6 @@ import { customersManager } from '../../dao/mongoose.customer.manager.js';
 
 export async function getCustomersController(req, res) {
     const queryParams = req.query;
-    const allUsers = await customersManager.getUsers();
+    const allUsers = await customersManager.getCustomers();
     res.json(allUsers)
 }
