341626c9b9cd567012c2502bf91371599fa89614
Edward Espinoza <em.esc.et@gmail.com>
2023-06-12 18:42:04 -0500

    Replaced head text to Global

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -6,5 +6,8 @@ export default session({
     store: MongoStore.create({ mongoUrl: MONGODB_CNX_STR }),
     saveUninitialized: false,
     resave: false,
-    secret: 'SESSION_SECRET'
+    secret: 'SESSION_SECRET',
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000 // 24 horas en milisegundos
+    }
 })
