0463bd32b5adfae1d4b738cf4a178aead2ad06f1
Edward Espinoza <em.esc.et@gmail.com>
2023-09-04 11:57:06 -0500

    Update token apisperu

--- src/functions/fetchDNIData.js
+++ src/functions/fetchDNIData.js
@@ -3,7 +3,7 @@
 export async function fetchData(dni) {
     try {
         const options = { method: 'GET' };
-        const response = await fetch(`https://dniruc.apisperu.com/api/v1/dni/${dni}?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVtLmVzYy5ldEBnbWFpbC5jb20ifQ.cdU8F_bc2jA_4ayDVmD5K4bKwN8lQ5sZtKXqBIZwwEM`, options);
+        const response = await fetch(`https://dniruc.apisperu.com/api/v1/dni/${dni}?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImN1ZW50YTBkb3RhMDFAZ21haWwuY29tIn0.MsCEc12bA-T3sDPaNK1d18OEGFfKP0MYY-dts_enw7k`, options);
         const data = await response.json();
         console.log(data);
         return data;
